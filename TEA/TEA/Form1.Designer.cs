98ced1675fce67adfc5a43bbb8f88ef883e9d996
anggadharma60 <anggadharma60>
2020-06-18 17:54:27 +0700

    Upload

---
+++ TEA/TEA/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace TEA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(117, 18);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(384, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Aplikasi Enkripsi-Dekripsi Metode TEA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Plaintext";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ciphertext";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(360, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Key";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(412, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(364, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(477, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 25);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear Plaintext";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(364, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 25);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Clear Ciphertext";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(477, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 25);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Clear Key";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(477, 163);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 25);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Clear All";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(364, 194);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 25);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Sample Plaintext";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(477, 194);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(95, 25);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Sample Key";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 61);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 96);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(99, 200);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(210, 96);
+            this.textBox3.TabIndex = 18;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(364, 250);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.richTextBox1.Size = new System.Drawing.Size(208, 46);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(364, 227);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(624, 321);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tiny Encryption Algorithm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TEA/TEA/TEA.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEA
+{
+	public class TEA
+    {
+		
+		// Ctor
+		public TEA()
+		{
+		}
+
+		public string Encrypt(string Data, string Key)
+		{
+			/*if (Data.Length == 0)
+			throw new ArgumentException("Data must be at least 1 character in length.");
+			*/
+			uint[] formattedKey = FormatKey(Key);
+
+			if (Data.Length % 2 != 0) Data += '\0'; // Make sure array is even in length.		
+			byte[] dataBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(Data);
+
+			string cipher = string.Empty;
+			uint[] tempData = new uint[2];
+			for (int i = 0; i < dataBytes.Length; i += 2)
+			{
+				tempData[0] = dataBytes[i];
+				tempData[1] = dataBytes[i + 1];
+				code(tempData, formattedKey);
+				cipher += Util.ConvertUIntToString(tempData[0]) + Util.ConvertUIntToString(tempData[1]);
+			}
+
+			return cipher;
+		}
+
+		public string Decrypt(string Data, string Key)
+		{
+			uint[] formattedKey = FormatKey(Key);
+
+			int x = 0;
+			uint[] tempData = new uint[2];
+			byte[] dataBytes = new byte[Data.Length / 8 * 2];
+			for (int i = 0; i < Data.Length; i += 8)
+			{
+				tempData[0] = Util.ConvertStringToUInt(Data.Substring(i, 4));
+				tempData[1] = Util.ConvertStringToUInt(Data.Substring(i + 4, 4));
+				decode(tempData, formattedKey);
+				dataBytes[x++] = (byte)tempData[0];
+				dataBytes[x++] = (byte)tempData[1];
+			}
+
+			string decipheredString = System.Text.ASCIIEncoding.ASCII.GetString(dataBytes, 0, dataBytes.Length);
+			if (decipheredString[decipheredString.Length - 1] == '\0') // Strip the null char if it was added.
+				decipheredString = decipheredString.Substring(0, decipheredString.Length - 1);
+			return decipheredString;
+		}
+
+		public uint[] FormatKey(string Key)
+		{
+			/*if (Key.Length == 0)
+				throw new ArgumentException("Key must be between 1 and 16 characters in length");
+			*/
+			Key = Key.PadRight(16, ' ').Substring(0, 16); // Ensure that the key is 16 chars in length.
+			uint[] formattedKey = new uint[4];
+
+			// Get the key into the correct format for TEA usage.
+			int j = 0;
+			for (int i = 0; i < Key.Length; i += 4)
+				formattedKey[j++] = Util.ConvertStringToUInt(Key.Substring(i, 4));
+
+			return formattedKey;
+		}
+
+		#region Tea Algorithm
+		public void code(uint[] v, uint[] k)
+		{
+			uint y = v[0];
+			uint z = v[1];
+			uint sum = 0;
+			uint delta = 0x9e3779b9;
+			uint n = 32;
+
+			while (n-- > 0)
+			{
+				sum += delta;
+				y += (z << 4) + k[0] ^ z + sum ^ (z >> 5) + k[1];
+				z += (y << 4) + k[2] ^ y + sum ^ (y >> 5) + k[3];
+			}
+
+			v[0] = y;
+			v[1] = z;
+		}
+
+		public void decode(uint[] v, uint[] k)
+		{
+			uint n = 32;
+			uint sum;
+			uint y = v[0];
+			uint z = v[1];
+			uint delta = 0x9e3779b9;
+
+			sum = delta << 5;
+
+			while (n-- > 0)
+			{
+				z -= (y << 4) + k[2] ^ y + sum ^ (y >> 5) + k[3];
+				y -= (z << 4) + k[0] ^ z + sum ^ (z >> 5) + k[1];
+				sum -= delta;
+			}
+
+			v[0] = y;
+			v[1] = z;
+		}
+		#endregion
+	}
+}
